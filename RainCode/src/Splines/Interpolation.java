9f7f903605e1846c23c3b95e7c10c1894cf4bf93
Felix Fischer <felix.ma.fischer@gmail.com>
2018-03-24 16:53:46 +0100

    added paint method to Interpolation

--- RainCode/src/Splines/Interpolation.java
+++ RainCode/src/Splines/Interpolation.java
@@ -1,5 +1,7 @@
 package Splines;
 
+import java.awt.*;
+
 public abstract class Interpolation {
 
     protected double[] x, y;
@@ -11,4 +13,17 @@ public abstract class Interpolation {
 
     public abstract double[] evaluate(double t);
 
+    public void paint(Graphics g, double resolution) {
+        int x_old = (int) this.x[0];
+        int y_old = (int) this.y[0];
+        int x_new = 0;
+        int y_new = 0;
+
+        for (double t = 0; t <= 1; t += resolution) {
+            double xy[] = this.evaluate(t);
+            x_new = (int) xy[0];
+            y_new = (int) xy[1];
+            g.drawLine(x_old, y_old, x_new, y_new);
+        }
+    }
 }
