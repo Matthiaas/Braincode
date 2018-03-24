4ff9ed69c1a1bf0d75d74ca012dbd5d56e1a77d2
Matthias <matthias.bungeroth@tum.de>
2018-03-24 14:03:11 +0100

    added functions to interface

--- RainCode/src/Parser.java
+++ RainCode/src/Parser.java
@@ -1,2 +1,14 @@
+import java.util.List;
+
 public interface Parser {
+
+    public default List<List<Position>> parseString( String code){
+        throw  new RuntimeException("Not Implemented yet");
+    }
+
+    public default List<List<Position>> parseFile(String filname){
+        throw  new RuntimeException("Not Implemented yet");
+    }
+
 }
+
