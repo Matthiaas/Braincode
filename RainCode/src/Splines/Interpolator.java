5502dd621b579190bbf9981d7a66ac6892645a6e
Matthias <matthias.bungeroth@tum.de>
2018-03-24 23:42:49 +0100

    bugfix

--- RainCode/src/Splines/Interpolator.java
+++ RainCode/src/Splines/Interpolator.java
@@ -31,11 +31,11 @@ public abstract class Interpolator {
             y_old = y_new;
             x_new = (int) xy[0];
             y_new = (int) xy[1];
-            //g.drawLine(x_old, y_old, x_new, y_new);
+            g.drawLine(x_old, y_old, x_new, y_new);
         }
 
         for (int i = 0; i < l.length(); i++) {
-            g.fillRect((int) l.getX()[i], (int) l.getY()[i], 5, 5);
+           // g.fillRect((int) l.getX()[i], (int) l.getY()[i], 5, 5);
         }
 
         return bufferedImage;
