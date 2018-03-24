aa795a6118c6e8501170a280560e60e0cd329af5
Sebastian Kappes <spam.kappes@tum.de>
2018-03-24 22:12:46 +0100

    Added new files for testing

---
+++ res/test5.c
@@ -0,0 +1,42 @@
+main () {
+	test();
+	da();
+	du();
+	gi();
+open();
+read();
+write();
+close();
+}
+void one () {
+open();
+test();
+da();
+du();
+gi();
+read();
+write();
+close();
+}
+
+void two () {
+open();
+read();
+write();
+close();
+test();
+da();
+du();
+gi();
+
+}
+void three () {
+open();
+read();
+test();
+da();
+du();
+gi();
+write();
+close();
+}
