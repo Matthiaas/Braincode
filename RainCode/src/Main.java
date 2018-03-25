68e1f5c1f99f6d839c1697f1076c352f029cce91
Sebastian Kappes <spam.kappes@tum.de>
2018-03-25 03:00:16 +0200

    removed name attribute from pint class

--- RainCode/src/Main.java
+++ RainCode/src/Main.java
@@ -103,6 +103,24 @@ public class Main extends JPanel {
         panel.repaint();
         */
 
+        int html_width = 690;
+        int html_hight = 538;
+        String html = "" +
+                "<img src=\"" + args[2] + "\" width=\"" + html_width + "\" height=\"" + html_hight + "\" alt=\"Karte\" usemap=\"#Landkarte\"> "
+                + "<map name=\"Methods\">";
+
+        List included
+        for (Line l : lines) {
+
+    <area shape =\"rect\" coords=\"11,10,80,46\"
+            href =\"http://www.koblenz.de/\" alt=\"Koblenz\" title=\"Koblenz\">
+        }
+
+               html = html + "</map>";
+
+
+
+
     }
 
     public void paint(Graphics g) {
--- RainCode/src/Splines/Point.java
+++ RainCode/src/Splines/Point.java
@@ -5,17 +5,15 @@ import cparse.GaussDistr;
 public class Point {
 
     double x, y, z;
-    String methodName;
 
-    public Point(double x, double y, double z, String methodName) {
+    public Point(double x, double y, double z) {
         this.x = x;
         this.y = y;
         this.z = z;
-        this.methodName = methodName;
     }
 
     public Point deviate(GaussDistr g, int count) {
         double[] xyz = g.distribute(x, y, z, count);
-        return new Point(xyz[0], xyz[1], xyz[2], methodName);
+        return new Point(xyz[0], xyz[1], xyz[2]);
     }
 }
