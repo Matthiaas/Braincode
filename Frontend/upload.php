c85687e45b0f88a003176123e82495025bf9e2c8
Floris Westermann <floris.westermann@yahoo.de>
2018-03-24 21:32:14 -0700

    frontend incominggit add *git add *

--- Frontend/upload.php
+++ Frontend/upload.php
@@ -1,14 +1,32 @@
+<!--
+ *
+ * quick and dirty code!
+ * DO NOT USE IN PRODUCTION ENVIRONMENT
+ *
+ * SECURITY HAZARD
+ *
+ * you WILL die
+ *
+-->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+
+
+    <title>RainCode</title>
+</head>
+<body style = "background-color: #1A1919; color: white">
+
+
 <?php
 $target_dir = "uploads/";
 $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
 $uploadOk = 1;
 $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
 
-// Check file size
-if ($_FILES["fileToUpload"]["size"] > 500000) {
-    echo "Sorry, your file is too large.";
-    $uploadOk = 0;
-}
+
 
 // Check if $uploadOk is set to 0 by an error
 if ($uploadOk == 0) {
@@ -16,13 +34,16 @@ if ($uploadOk == 0) {
 // if everything is ok, try to upload file
 } else {
     if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-        echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
+
     } else {
         echo "Sorry, there was an error uploading your file.";
     }
 }
 
-/*exec('java -jar RainCodeMain.jar ' . $target_file);
-readfile('output/image.jpg');*/
 
+echo (exec('java -jar RainCode.jar ' . $target_file . ' output.png'));
+echo "<br><a style='color:white' href='out/output.png' download> Download Image in full quality </a>";
 ?>
+
+</body>
+</html>
